9c52301ba59db7cfef55624ba3aa1f468384be09
Enes Akbulut <107497616+Enes-Akbulut@users.noreply.github.com>
2022-06-23 13:56:30 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+   double pi= 3.14,r,a, formul,alan,cevre;
+
+        Scanner girdi = new Scanner(System.in);
+        
+        System.out.print("YARI CAPINI GIRINIZ =  ");
+        r =girdi.nextDouble();
+
+        System.out.print("YARI CAPINI GIRINIZ =  ");
+        a =girdi.nextDouble();
+        alan=(pi*r*r);
+        cevre=2*pi*r;
+        formul = ((pi*(r*r)*a)/360);
+        System.out.println(formul);
+        System.out.println("DAIRENIN ALANI"+cevre);
+        System.out.println("DAIRENIN DILIM ALANI"+formul);
+        System.out.println("DAIRENIN TAM ALANİ" + alan);
+
+
     }
 }
